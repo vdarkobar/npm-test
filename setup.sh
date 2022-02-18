8aca845b322116fae7b8520de0865e84bd59d1b0
Darko Vukovic <vdarkobar@gmail.com>
2022-02-18 12:13:05 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+db_root_pwd=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 25); echo ${db_root_pwd} > secrets/db_root_pwd.secret && \
+mysql_pwd=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 25); echo ${mysql_pwd} > secrets/mysql_pwd.secret && \
+sudo chown -R root:root secrets/ && \
+sudo chmod -R 600 secrets/
