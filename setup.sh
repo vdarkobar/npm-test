228edadafba98826a7ae8689c3fcbea19c232f8e
Darko Vukovic <vdarkobar@gmail.com>
2023-09-29 18:59:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 clear
 echo -ne "${GREEN}Enter Time Zone: "; read TZONE; \
 echo -ne "${GREEN}Enter NPM Port Number: "; read PORTN; \
-echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 25 > secrets/db_root_pwd.secret && \
-echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 25 > secrets/mysql_pwd.secret && \
+echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 32 > .secrets/db_root_pwd.secret && \
+echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 32 > .secrets/mysql_pwd.secret && \
 sed -i "s|01|${TZONE}|" .env && \
 sed -i "s|02|${PORTN}|" .env && \
 rm README.md && \
 rm letsencrypt/tmp && \
 sudo rm -rf shared/ && \
-sudo chown -R root:root secrets/ && \
-sudo chmod -R 600 secrets/ && \
+sudo chown -R root:root .secrets/ && \
+sudo chmod -R 600 .secrets/ && \
 while true; do
     read -p "Execute 'docker compose up -d' now? (y/n)" yn
     case $yn in
